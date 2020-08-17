ee05566a287f4b7a9906217ce68f6e4ff61f35d8
Griffin Boyd <grifboyd@gmail.com>
2020-08-17 18:00:24 -0400

    select checkboxes and save

--- nano_bucketing.py
+++ nano_bucketing.py
@@ -71,6 +71,9 @@ try:
             for i in cursor.description:
                 column_names.append(i[0])
             result.append(column_names)
+            not_bucketed.append(column_names)
+            expired.append(column_names)
+
             for row in rows:
                 result.append(row)
 
@@ -89,8 +92,12 @@ except:
 finally:
     db.close()
 
+# TO DO: analyze and print bucket info
+
 # User has to install webdriver
 browser = webdriver.Firefox()
+
+# These will be called once
 
 def nano_login():
     browser.get("https://tools.nanonation.net/")
@@ -101,34 +108,40 @@ def nano_login():
 def client_select():
     digital_signage = browser.find_element_by_css_selector("#ctl00_ContentPlaceHolder_Main_ConsoleNav1_Inner_dvSignage21")
     digital_signage.click()
-    #client = browser.find_element_by_css_selector("tr.alternate:nth-child(12) > td:nth-child(1) > a:nth-child(1)")
     client = browser.find_element_by_link_text(client_name)
     client.click()
 
 def load_all_media():
     browser.find_element_by_css_selector('#ctl00_ContentPlaceHolder_Main_ViewStyleList').click()
+    # TO DO: implement webdriverwait?
     browser.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder_Main_MediaDialogMaximumResults"]/option[4]').click()
+    rows_count = browser.execute_script("return document.getElementsByTagName('tr').length")
+    print(rows_count)
+
+# These will be looped over n times (n = number of media groups)
 
 def load_all_media_groups():
     browser.find_element_by_css_selector("td.tab:nth-child(5)").click()
+    # TO DO: implement webdriverwait?
     browser.find_element_by_xpath('/html/body/form/div[3]/div[3]/div[2]/div/div/select/option[4]').click()
     
-def select_media_group():
-    browser.find_element_by_link_text('TEST MEDIA GROUP').click()
+def select_media_group(media_group):
+    browser.find_element_by_link_text(media_group).click()
 
 def select_track(filename):
     pass
-    # TO DO: Get path to parent element from child's <td> text, click associated checkbox
-        # If this is a wild goose chase, maybe keep media in icon layout rather than list
-            # Icons have more ideal structure with filename = title, but checkboxes are hidden
-        # Possibly need to scroll stuff into view:
-            # browser.execute_script("arguments[0].scrollIntoView();", ELEMENT)
+    track = browser.find_element_by_xpath('//tr/td[(contains(text(),'+ '"' + filename + '"'+'))]/../td/input[@class="check_file"]')
+    track.click()
 
 def add_media():
-    browser.find_element_by_css_selector("#ctl00_ContentPlaceHolder_Main_AddToMediaGroup").click()
-    for f in filenames:
-        select_track(f)
-    # TO DO: submit, add to media group, save
+    if browser.find_element_by_css_selector('#ctl00_ContentPlaceHolder_Main_mediaGroupItems > tbody > tr.empty_row > td > span > center'):
+        browser.find_element_by_css_selector('#ctl00_ContentPlaceHolder_Main_AddToMediaGroup').click()
+        for f in filenames:
+            select_track(f)
+        browser.find_element_by_css_selector('#ctl00_ContentPlaceHolder_Main_SelectMediaItemDialog_SaveButton').click()
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'ctl00_ContentPlaceHolder_Main_insertAtItems_ctl01_expandingRow'))).click()
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'ctl00_ContentPlaceHolder_Main_mediaGroupItems_0_EditMediaGroupItemLink')))
+        browser.find_element_by_css_selector('#ctl00_ContentPlaceHolder_Main_saveButton').click()
 
 # For testing: export CSV of 
 # TO DO: export not_bucketed, expired csvs if lists are not empty
@@ -142,8 +155,9 @@ with open(origin_csv_file_path, 'w', newline='') as csvfile:
 nano_login()
 client_select()
 load_all_media()
+# set media group, loop through until it changes
 load_all_media_groups()
-select_media_group()
+select_media_group('TEST MEDIA GROUP')
 add_media()
 
 # browser.quit()
